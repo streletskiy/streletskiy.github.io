f043de761b5b5e0f249e257910a3b75ad95a88c6
streletskiy <nortondeicide@gmail.com>
2025-04-08 15:58:00 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://streletskiy.github.io',
+	site: 'https://streletskiy.com',
 	integrations: [
 		starlight({
 			title: 'streletskiy/docs',
